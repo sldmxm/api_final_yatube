fe1e683697dcbc494b3fd061ca2f31ef690c9fef
Maxim Solodilov <sldmxm@gmail.com>
2022-11-05 19:40:27 +0700

    before 1st review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import IsAuthenticated
 
-from posts.models import Post, Group, Follow, User
+from posts.models import Post, Group, Follow
 from api.serializers import (
     PostSerializer, CommentSerializer, GroupSerializer, FollowSerializer
 )
